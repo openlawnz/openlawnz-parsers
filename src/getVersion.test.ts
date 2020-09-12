90603a61857568b2b655349076e70592ec86b8f1
William Parry <williamparry@gmail.com>
2020-09-13 08:42:51 +1000

    Bump version for npm. Temporarily skip some tests that are working on Windows only.

--- src/getVersion.test.ts
+++ src/getVersion.test.ts
@@ -8,6 +8,6 @@ import getVersion from './getVersion';
 
 describe('getVersion', () => {
     it('Should return correct version', async () => {
-        expect(getVersion()).toBe('1.0.0');
+        expect(getVersion()).toBe('1.0.1');
     });
 });
